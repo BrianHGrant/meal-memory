4da1420d947fae50030bb38c02e4a3f1e359909e
BrianHGrant <bhgrant8@gmail.com>
2016-08-12 00:09:45 -0700

    add code to support display of meal component

--- app/meal-list.component.ts
+++ app/meal-list.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter} from 'angular2/core';
 import { Meal } from './meal.model';
+import { MealComponent} from './meal.component'
 
 @Component ({
   selector: 'meal-list',
   inputs: ['mealList'],
   outputs: ['onMealSelect'],
-  directives: [],
+  directives: [MealComponent],
   template: `
   <meal-display *ngFor="#currentMeal of mealList"
     [class.selected]="currentMeal===selectedMeal"
@@ -17,6 +18,7 @@ import { Meal } from './meal.model';
 
 export class MealListComponent {
   public mealList: Meal[];
+  public selectedMeal: Meal;
   public onMealSelect: EventEmitter<Meal>;
   constructor(){
     this.onMealSelect = new EventEmitter();
@@ -24,5 +26,6 @@ export class MealListComponent {
   mealClicked(clickedMeal: Meal): void {
     this.selectedMeal = clickedMeal;
     this.onMealSelect.emit(clickedMeal);
+    console.log(this.selectedMeal);
   }
 }
