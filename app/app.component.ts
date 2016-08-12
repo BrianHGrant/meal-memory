a6f2b19fb021bb10c51965f1f747a44d7eaebbb5
BrianHGrant <bhgrant8@gmail.com>
2016-08-11 23:41:27 -0700

    add meal model to application

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,16 +1,24 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
+import { MealListComponent} from './meal-list.component'
 
 @Component({
   selector: 'my-app',
-
+  directives: [MealListComponent],
   template: `
   <div class="container">
     <h1>Meal Memory Fitness Tracker</h1>
-
+    <meal-list [meal-list]="meals"></meal-list>
   </div>
   `
 })
 
 export class AppComponent {
-
+  public meals: Meal[];
+  constructor(){
+    this.meals =
+    [
+      new Meal("Chicken Alfredo", 600, "1 cup, homemade w/ organic chicken, artichoke hearts")
+    ]
+  }
 }
